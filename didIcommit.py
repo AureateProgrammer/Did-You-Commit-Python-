34de28ca35f9ba8f67b7e9a99f69006a67e3ccd2
AureteProgrammer <CodingBlakeMorris@gmail.com>
2026-08-02 19:37:41 -0400

    Creating username variable and defining the url

--- didIcommit.py
+++ didIcommit.py
@@ -2,6 +2,8 @@ import subprocess
 import requests
 
 response = requests.get(url=10)
+username ="AureateProgrammer"#creating a variable for my username
+url = f"https://api.github.com/users/{username}/events/public"#this line is building the adress connecting,
 
-
+request.get (url)
 
