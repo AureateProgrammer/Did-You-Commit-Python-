91a8b712321923f7413a722419c6229d291492f1
AureteProgrammer <CodingBlakeMorris@gmail.com>
2026-08-02 19:57:50 -0400

    finishing commiting logic

--- didIcommit.py
+++ didIcommit.py
@@ -7,4 +7,18 @@ url = f"https://api.github.com/users/{username}/events/public"
 response = requests.get(url)
 events = response.json()
 
-print(events[0])
+today = str(date.today())
+commited_today = False
+
+for event1 in events:
+    if event1["type"] =='PushEvent' and event1["created_at"].startswith(today):
+        commited_today = True
+if commited_today == True:
+    print ('Yay')
+else:
+    print('noo')
+    
+
+
+print(today )
+print(events[1])
